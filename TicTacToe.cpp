b0baa47339151cb834ddb7b3a3961c3e88f4193e
sap586 <sap586@nyu.edu>
2017-12-21 14:09:21 -0500

    TicTacToe

---
+++ TicTacToe.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+char matrix[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char player = 'X';
+
+void Draw() {
+//    system("CLS");
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void changePlayer() {
+    if (player == 'X') {
+        player = 'O';
+    } else {
+        player = 'X';
+    }
+}
+
+void Input() {
+    int a;
+    cout<<"Enter a number : ";
+    cin>>a;
+    if (a == 1) {
+        if (matrix[0][0] == '1') {
+            matrix[0][0] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 2) {
+        if (matrix[0][1] == '2') {
+            matrix[0][1] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 3) {
+        if (matrix[0][2] == '3') {
+            matrix[0][2] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 4) {
+        if (matrix[1][0] == '4') {
+            matrix[1][0] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 5) {
+        if (matrix[1][1] == '5') {
+            matrix[1][1] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 6) {
+        if (matrix[1][2] == '6') {
+            matrix[1][2] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 7) {
+        if (matrix[2][0] == '7') {
+            matrix[2][0] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 8) {
+        if (matrix[2][1] == '8') {
+            matrix[2][1] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else if (a == 9) {
+        if (matrix[2][2] == '9') {
+            matrix[2][2] = player;
+        } else {
+            cout<<"Invalid Input\n";
+            return;
+        }
+    } else {
+        cout<<"Invalid Input\n";
+        return;
+    }
+    changePlayer();
+}
+
+char Win() {
+    if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X') {
+        return 'X';
+    }
+    if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X') {
+        return 'X';
+    }
+    if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X') {
+        return 'X';
+    }
+    if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X') {
+        return 'X';
+    }
+    if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X') {
+        return 'X';
+    }
+    if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X') {
+        return 'X';
+    }
+    if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+        return 'X';
+    }
+    if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
+        return 'X';
+    }
+    
+    if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O') {
+        return 'O';
+    }
+    if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O') {
+        return 'O';
+    }
+    if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O') {
+        return 'O';
+    }
+    if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O') {
+        return 'O';
+    }
+    if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O') {
+        return 'O';
+    }
+    if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O') {
+        return 'O';
+    }
+    if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
+        return 'O';
+    }
+    if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
+        return 'O';
+    }
+    
+    return '/';
+}
+
+int main(int argc, char** argv) {
+    Draw();
+    while(1) {
+        Input();
+        Draw();
+        if (Win() == 'X') {
+            cout<<"X Wins";
+            break;
+        }
+        if (Win() == 'O') {
+            cout<<"Y Wins";
+            break;
+        }
+    }
+    return 0;
+}
